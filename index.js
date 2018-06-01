13c579b01dc27e1e407dcbbfa11dac611ee44381
rhuairahrighairigh <ruaridh.odonnell@gmail.com>
2018-06-01 15:11:21 +0100

    add sendPayment ability

--- index.js
+++ index.js
@@ -10,40 +10,53 @@ class KavaPlugin extends PluginPayment {
     //TODO fill out all opts here
     // role: client|server
     // address: kavaAddress
-    // kavaServer: url:port
+    // kavaNodeURI: url:port
     // kavaAccountName
     // kavaAccountPassword
+    // kavaClientURI
     super(opts)
-    this.kavaServer = RpcClient(opts.kavaServerAddress || 'ws://kava.connector.kava.io:46657')
+    this.kavaNode = RpcClient(opts.kavaNodeURI || 'ws://kava.connector.kava.io:46657')
     this.address = opts.address
     this.kavaAccountName = opts.kavaAccountName
     this.kavaAccountPassword = opts.kavaAccountPassword
+    this.kavaClientURI = opts.kavaClientURI
   }
 
   async connectPayment () {
     //TODO setup account through LCD if not there already
-    await this.kavaServer.subscribe({query: "tm.event='Tx'"}, this.txEventHandler)
+    await this.kavaNode.subscribe({query: "tm.event='Tx'"}, this.txEventHandler)
     debug('Succesfully subscribed to Kava node.')
   }
 
   async sendPayment (details, amount) {
     //TODO set up LCD on server
-    //TODO implement this
-      
-    // var requestOptions = {
-//         method: 'POST',
-//         uri: this.kavaServer+`/accounts/${details.address}/send`,
-//         body: {
-//           amount: amount,
-//           name: this.kavaAccountName,
-//           password: this.kavaAccountPassword,
-//           chain_id: 'kava',
-//           sequence: 0 //TODO how will this work?
-//         },
-//         json: true // Automatically stringifies the body to JSON
-//     };
-//
-//     let parsedBody = await request(requestOptions)
+    
+    // Get sequence number for sending account.
+    let response = await request({
+        method: 'GET',
+        uri: this.kavaClientURI+`/accounts/${this.address}`,
+        json: true // Automatically stringifies the body to JSON
+    })
+    let sequenceNumber = parseInt(response.value.sequence)
+    debug(sequenceNumber)
+
+    // Send payment.
+    let paymentRequestOptions = {
+        method: 'POST',
+        uri: this.kavaClientURI+`/accounts/${details.address}/send`,
+        body: {
+          amount: [
+            {denom: "kavaToken", "amount": amount}
+          ],
+          name: this.kavaAccountName,
+          password: this.kavaAccountPassword,
+          chain_id: 'kava',
+          sequence: sequenceNumber,
+        },
+        json: true
+    };
+
+    return await request(paymentRequestOptions)
   }
 
   async getPaymentDetails (userId) {
